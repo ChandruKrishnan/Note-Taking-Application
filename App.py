dedab96c3bc859f5f291e0ed76a90a0ca22b855a
Chandrashekar Krishnan <rjuno18@gmail.com>
2024-01-09 12:35:08 -0600

    added flask CORS to app.py

--- App.py
+++ App.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:notesApp?@localhost/notesdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
