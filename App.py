619e6f40e2980cdeb47c9a7cd77c4e7d8761c927
Chandrashekar Krishnan <rjuno18@gmail.com>
2024-01-09 10:35:37 -0600

    removed graphql integration in app.py

--- App.py
+++ App.py
@@ -20,11 +20,6 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    user = db.relationship('User', backref=db.backref('notes', lazy=True))
-
-    def __repr__(self):
-        return f'<Note {self.title}>'
 
 @app.route('/api/notes', methods=['GET'])
 def get_notes():
